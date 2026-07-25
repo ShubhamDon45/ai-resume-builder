83f5050e0b035d7b15e01b629f0cc186fd88fc86
shubhammirkale <mirkaleshubham06@gmail.com>
2026-07-26 02:44:44 +0530

    Update server.js

--- resume-builder/server.js
+++ resume-builder/server.js
@@ -1,8 +1,4 @@
 require('dotenv').config();
-const app = require('./app');
+const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
-});
+module.exports = app;
