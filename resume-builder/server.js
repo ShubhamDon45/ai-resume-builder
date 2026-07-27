f91b7254bce76bf6e94ef06c315cff26146e4faf
Jay Kalane <jaikalane2020@gmail.com>
2026-07-27 21:43:46 +0530

    Update server.js to work with Vercel - handle standalone execution

--- resume-builder/server.js
+++ resume-builder/server.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
 const app = require("./app");
 
+const PORT = process.env.PORT || 3000;
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
 module.exports = app;
